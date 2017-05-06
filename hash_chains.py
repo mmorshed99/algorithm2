6ef978d2af6951279f9a049b951d64b55117d1b4
mmorshed99 <mmorshed@asu.edu>
2017-05-06 18:39:39 -0400

    adding phone book and hash chain problem

---
+++ hash_chains.py
@@ -0,0 +1,100 @@
+# python3
+
+class Query:
+
+    def __init__(self, query):
+        self.type = query[0]
+        if self.type == 'check':
+            self.ind = int(query[1])
+        else:
+            self.s = query[1]
+
+
+class QueryProcessor:
+    _multiplier = 263
+    _prime = 1000000007
+
+    def __init__(self, bucket_count):
+        self.bucket_count = bucket_count
+        # store all strings in one list
+        self.elems = {}
+
+    def _hash_func(self, s):
+        ans = 0
+        for c in reversed(s):
+            ans = (ans * self._multiplier + ord(c)) % self._prime
+        return ans % self.bucket_count
+
+    def write_search_result(self, was_found):
+        print('yes' if was_found else 'no')
+
+    def write_chain(self, chain):
+        print(' '.join(chain))
+
+    def read_query(self):
+        return Query(input().split())
+
+    def process_query(self, query):
+        #hash_index = self._hash_func
+        if query.type == "check":
+            # use reverse order, because we append strings to the end
+            #self.write_chain(cur for cur in reversed(self.elems)
+                        #if self._hash_func(cur) == query.ind)
+           #print (query.ind)
+           #print(self.elems[query.ind])
+           if query.ind in self.elems:
+             #print(22)
+             print(self.elems[query.ind])
+           else:
+             print(' ') 
+
+        else:
+            #try:
+                #ind = self.elems.index(query.s)
+            #except ValueError:
+                #ind = -1
+            if query.type == 'find':
+                hash_index = self._hash_func(query.s)
+                if hash_index in self.elems:
+                  pieces = self.elems[hash_index].split()
+                  if query.s in pieces:
+                    print('yes')
+                  else:
+                    print('no')
+                else:
+                    print('no')
+
+                #self.write_search_result(ind != -1)
+            elif query.type == 'add':
+                hash_index = self._hash_func(query.s)
+                #print(hash_index)
+                if hash_index in self.elems:
+                  pieces = self.elems[hash_index].split()
+                  #print(pieces)
+                  #print(query.s)
+                  if query.s not in pieces:
+                    pieces.insert(0,query.s)
+                    self.elems[hash_index] = ' '.join(pieces)
+                else:
+                    self.elems[hash_index]=query.s
+                #if ind == -1:
+                    #self.elems.append(query.s)
+            else:
+                #if ind != -1:
+                    #self.elems.pop(ind)
+                hash_index = self._hash_func(query.s)
+                if hash_index in self.elems:
+                  pieces = self.elems[hash_index].split()
+                  if query.s in pieces:
+                    pieces.remove(query.s)
+                  self.elems[hash_index] = ' '.join(pieces)
+
+    def process_queries(self):
+        n = int(input())
+        for i in range(n):
+            self.process_query(self.read_query())
+
+if __name__ == '__main__':
+    bucket_count = int(input())
+    proc = QueryProcessor(bucket_count)
+    proc.process_queries()
---
+++ phone_book.py
@@ -0,0 +1,58 @@
+# python3
+import sys
+
+class Query:
+    def __init__(self, query):
+        self.type = query[0]
+        self.number = int(query[1])
+        if self.type == 'add':
+            self.name = query[2]
+
+def read_queries():
+    n = int(input())
+    return [Query(input().split()) for i in range(n)]
+
+def write_responses(result):
+    print('\n'.join(result))
+
+def process_queries(queries):
+    result = []
+    # Keep list of all existing (i.e. not deleted yet) contacts.
+    contacts = {}
+    for cur_query in queries:
+        if cur_query.type == 'add':
+            # if we already have contact with such number,
+            # we should rewrite contact's name
+            #for contact in contacts:
+                #if contact.number == cur_query.number:
+                    #contact.name = cur_query.name
+                    #break
+            #else: # otherwise, just add it
+                #contacts.append(cur_query)
+            #if contacts.has_key(cur_query.number):
+            contacts[cur_query.number] = cur_query.name
+            #else:
+        elif cur_query.type == 'del':
+            #for j in range(len(contacts)):
+                #if contacts[j].number == cur_query.number:
+                    #contacts.pop(j)
+                    #break
+            #if contacts.has_key(cur_query.number):
+            if cur_query.number in contacts:
+              del contacts[cur_query.number]
+        else:
+            response = 'not found'
+            #for contact in contacts:
+                #if contact.number == cur_query.number:
+                    #response = contact.name
+            #if contacts.has_key(cur_query.number):
+            if cur_query.number in contacts:
+
+               response = contacts[cur_query.number]
+                    #break
+            result.append(response)
+    return result
+
+if __name__ == '__main__':
+    write_responses(process_queries(read_queries()))
+
