ec6f13bf391e26455e94d0164107cabcf9476398
mmorshed99 <mmorshed@asu.edu>
2017-05-06 18:36:22 -0400

    adding build heap and job queue

---
+++ build_heap.py
@@ -0,0 +1,121 @@
+# python3
+import math
+import sys
+class HeapBuilder:
+  def __init__(self):
+    self._swaps = []
+    self._data = []
+
+  def ReadData(self):
+    input = sys.stdin.read()
+    self._data = list(map(int, input.split()))
+    n = self._data[0]
+    self._data = self._data[1:]
+    assert n == len(self._data)
+
+  def WriteResponse(self):
+
+    print(len(self._swaps))
+    for swap in self._swaps:
+      print(swap[0], swap[1])
+
+  def GenerateSwaps(self):
+
+     number = len(self._data)
+
+     levels = (math.log(number)/math.log(2))
+     if levels == math.ceil(levels):
+       levels = int(levels)
+       p =  math.pow(2,levels)
+     else:
+       levels = math.ceil(levels)
+       levels = int(levels)
+     
+       p =  math.pow(2,levels)//2
+     p = p-1
+     p = int(p)
+     
+     q = number
+     while (p !=q) and (p>=0) :
+      for i in range(p ,q):
+        if i%2 == 1 and i !=number :
+         if(i+1 < number):
+          if (self._data[i] < self._data[i+1]):
+            if self._data[i] < self._data[(i-1)//2]:
+               self._swaps.append(((i-1)//2,i))
+               self._data[i], self._data[(i-1)//2] = self._data[(i-1)//2], self._data[i]
+               r = 2 * i + 1
+               j = i
+               #t = r + 1
+               while r < number:
+                    if ((r+1 < number) and  (self._data[r] < self._data[r+1]) and (self._data[r] < self._data[j])) or ((r+1 >= number) and (self._data[r] < self._data[j])):  
+
+                      self._swaps.append((j,r))
+                      self._data[r], self._data[j] = self._data[j], self._data[r]
+                      j = r 
+                      r = 2 * r + 1
+                   
+                    elif (r+1 <number) and self._data[r+1] < self._data[j]: 
+                      self._swaps.append((j,r+1))
+                      self._data[r+1], self._data[j] = self._data[j], self._data[r+1]
+                      j = r+1
+                      r = 2 * j + 1
+                    else:
+                      break     
+          else:
+            if (self._data[i+1] < self._data[(i-1)//2]):
+               self._swaps.append(((i-1)//2,i+1))
+               self._data[i+1], self._data[(i-1)//2] = self._data[(i-1)//2], self._data[i+1]
+               r = 2* (i+1) + 1
+               j = i+1
+               while r < number:
+                    if ((r+1 < number) and  (self._data[r] < self._data[r+1]) and (self._data[r] < self._data[j])) or ((r+1 >= number) and (self._data[r] < self._data[j])):
+
+                      self._swaps.append((j,r))
+                      self._data[r], self._data[j] = self._data[j], self._data[r]
+                      j = r 
+                      r = 2 * r + 1
+                    elif (r+1 <number) and self._data[r+1] < self._data[j]: 
+                      self._swaps.append((j,r+1))
+                      self._data[r+1], self._data[j] = self._data[j], self._data[r+1]
+                      j = r+1
+                      r = 2 * j + 1
+                    else:
+                      break
+         else:
+           if self._data[i] < self._data[(i-1)//2]:
+               self._swaps.append(((i-1)//2,i))
+               self._data[i], self._data[(i-1)//2] = self._data[(i-1)//2], self._data[i]
+               r = 2 * i + 1
+               j = i
+               
+               while r < number:
+                    if ((r+1 < number) and  (self._data[r] < self._data[r+1]) and (self._data[r] < self._data[j])) or ((r+1 >= number) and (self._data[r] < self._data[j])): 
+                    #if self._data[r] < self._data[j]:
+                      self._swaps.append((j,r,))
+                      self._data[r], self._data[j] = self._data[j], self._data[r]
+                      j = r 
+                      r = 2 * r + 1
+                   
+                    elif (r+1 <number) and self._data[r+1] < self._data[j]: 
+                      self._swaps.append((j,r+1))
+                      self._data[r+1], self._data[j] = self._data[j], self._data[r+1]
+                      j = r+1
+                      r = 2 * j + 1
+                    else:
+                     break  
+
+      p = (p-1)//2
+
+      q = p * 2 
+
+
+
+  def Solve(self):
+    self.ReadData()
+    self.GenerateSwaps()
+    self.WriteResponse()
+
+if __name__ == '__main__':
+    heap_builder = HeapBuilder()
+    heap_builder.Solve()
---
+++ job_queue.py
@@ -0,0 +1,83 @@
+# python3
+import sys
+
+class JobQueue:
+    def read_data(self):
+        #self.num_workers, m = map(int, input().split())
+        #self.jobs = list(map(int, input().split()))
+        #assert m == len(self.jobs)
+        input = sys.stdin.read()
+        self.data = list(map(int, input.split()))
+        self.num_workers = self.data[0]
+        m= self.data[1]
+        self.jobs = self.data[2:]
+        assert m == len(self.jobs)
+
+    def write_response(self):
+        for i in range(len(self.jobs)):
+          print(self.assigned_workers[i], self.start_times[i]) 
+
+    def assign_jobs(self):
+        def find_smaller(left, right,tree_left, tree_right, prio_tree_left, prio_tree_right):
+            
+            if prio_tree_left < prio_tree_right:
+              return left
+            if prio_tree_right < prio_tree_left:
+              return right
+            if prio_tree_left == prio_tree_right:
+              if tree_left < tree_right:
+                return left
+              elif tree_right < tree_left:
+                return right
+            return -1 
+        prio = []
+        index = []
+        tree = []
+        self.assigned_workers = [None] * len(self.jobs)
+        self.start_times = [None] * len(self.jobs)
+        for i in range(0,self.num_workers):
+           prio.append(0)
+           index.append(i)
+           tree.append(i)
+        
+
+        for i in range(len(self.jobs)):
+           self.assigned_workers[i] = tree[0]
+           self.start_times[i] = prio[tree[0]]
+           prio[tree[0]] += self.jobs[i]
+           current = 0
+           left = 0
+           right = 0
+           if 2*current + 1 < self.num_workers:
+             left = 2*current +1
+           if 2*current + 2 < self.num_workers:
+             right = 2*current + 2
+           next = find_smaller(left, right, tree[left], tree[right], prio[tree[left]], prio[tree[right]])
+           while (current != next and next != -1):
+                while ((prio[tree[current]] > prio[tree[next]]) or ((prio[tree[current]] == prio[tree[next]]) and (tree[current] > tree[next]))):
+                      tree[current], tree[next] = tree[next], tree[current]
+                      current = next
+                      left = current
+                      right = current
+                      if  2*current + 1 < self.num_workers:
+                         left = 2*current +1
+                      if 2*current + 2 < self.num_workers:
+                         right = 2*current + 2
+                      next = find_smaller(left, right, tree[left], tree[right], prio[tree[left]], prio[tree[right]])
+                      if current == next or next == -1:
+                        break
+
+                break
+
+
+
+                    
+    def solve(self):
+        self.read_data()
+        self.assign_jobs()
+        self.write_response()
+
+if __name__ == '__main__':
+    job_queue = JobQueue()
+    job_queue.solve()
+
