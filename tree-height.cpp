cba56405e1283db83b39977a13b3b998eabfc73f
mmorshed99 <mmorshed@asu.edu>
2017-05-23 15:23:25 -0400

    added comments

--- tree-height.cpp
+++ tree-height.cpp
@@ -24,26 +24,28 @@ public:
 
     for(int j = 0; j<n; j++)
       {
-	size[j] = -1;
-	height[j] = 1;
+	size[j] = -1;   // stores number of children for each element
+	height[j] = 1;  //intial height is 1 for all
       }
     for(int j =0; j< n; j++)
 
       {
 	if(parent[j] == -1)
 	  {
-	    root = j;
+	    root = j;   //root element has doesn't have parent
 
 	  }
 	else
 	  {
-	    size[parent[j]] += 1;
-	    child[parent[j]][size[parent[j]]] = j;
+	    size[parent[j]] += 1;  // counting number of elements each node is parenting
+	    child[parent[j]][size[parent[j]]] = j; // saving the list of children for each parent in a 2D array
 
 	  }
 
       }
     int p = -1;
+    // getting all lowest level elements
+
     for (int j = 0;j<n;j++)  {
       if (size[j] == -1){
 	p++;
@@ -51,14 +53,18 @@ public:
       }
     }
 
+    // trying to get the height .... checking if root has at least one child
     while(size[root] > -1){
       int k = -1;
       int temp[n];
+      // will iterate through of elements of same level..starting from lowest level
       for (int j=0;j<=p;j++) {
+	//if height of parent is less than child...height of parent become height of child + 1
 	if(height[parent[lowest[j]]] <= (height[lowest[j]] )) {
 	  height[parent[lowest[j]]] = height[lowest[j]] + 1;
 	}
         size[parent[lowest[j]]] -= 1; 
+        // When no more child left for the parent, it will be considered as child in next iteration
 	if(size[parent[lowest[j]]] == -1){
 	  k++;
 	  temp[k] = parent[lowest[j]];
@@ -66,12 +72,13 @@ public:
 
       }    
       p = k;
-
+      // parents from previous iteration were saved to be copied as new children to iterate over
       for (int c= 0; c<= k; c++){
 	lowest[c] = temp[c];
       }    
 
     }   
+    // height of the tree is same as height of the root starting from the bottom
     maxHeight = height[root];
     return maxHeight;
   }
